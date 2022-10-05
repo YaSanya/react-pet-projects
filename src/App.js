35d4d1c9c3c7431ef2e2e4cf6cfe7f8888310e73
nekkkos <alexdajneko22@gmail.com>
2022-10-05 11:44:08 +0200

    Users list

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
 import React from 'react';
 import './index.scss';
+import { Success } from './components/Success';
+import { Users } from './components/Users';
 
-const Modal = ({ open, setOpen, children }) => (
-  <div className={`overlay animated show' ${open ? 'show' : ''}`}>
-    <div className="modal">
-      <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
-        <title />
-        <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-      </svg>
-      {children}
-    </div>
-  </div>
-)
+// Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([]);   //[0] - name
+  const [invites, setInvites] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(true);
+  const [success, setSuccess] = React.useState(false);
+  const [searchValue, setsearchValue] = React.useState('');
 
-  const [open, setOpen] = React.useState(false);
+  React.useEffect(() => {   //отправка запроса на backend
+    fetch('https://reqres.in/api/users')
+      .then(res => res.json())  //если получен успешный ответ преобразуем его в json
+      .then(json => {
+        setUsers(json.data);
+      }).catch(err => {
+        console.warn(err);
+        alert('Ошибка при получении пользователей');
+      }).finally(() => setLoading(false));
+  }, []);
+
+  const onChangeSearchValue = (event) => {
+    setsearchValue(event.target.value);
+  };
+
+  const onClickInvite = (id) => {
+    console.log(id);
+    if (invites.includes(id)) {
+      setInvites(prev => prev.filter(_id => _id !== id));
+    } else {
+      setInvites((prev) => [...prev, id]);
+    }
+  };
+
+  const onClickSendInvites = () => {
+    setSuccess(true);   //меняем значение Success
+  }
 
   return (
     <div className="App">
-      <button onClick={() => setOpen(true)} className="open-modal-btn">✨ Открыть окно</button>
-      <Modal open={open} setOpen={setOpen}>
-        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-      </Modal>
+      {   // если success==true, рендерим success, иначе users
+        success ? (<Success count={invites.length}/>) : (
+          <Users
+          onChangeSearchValue={onChangeSearchValue}
+          searchValue={searchValue}
+          items={users}
+          isLoading={isLoading}
+          invites={invites}
+          onClickInvite={onClickInvite}
+          onClickSendInvites={onClickSendInvites}
+          />
+        )
+      }
     </div>
   );
 }
